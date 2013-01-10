6223e9017a7695f6c0b4536dd9b2e985e607926f
Germano Nicolini <germanogmn1@gmail.com>
2013-01-09 22:48:26 -0200

    camera zoom centered

--- Foundation/Camera/Camera2D.cs
+++ Foundation/Camera/Camera2D.cs
@@ -53,10 +53,17 @@ namespace Foundation.Camera
             var viewport = Game.GraphicsDevice.Viewport;
             translateCenterDisplay = new Vector3(viewport.Width / 2, viewport.Height / 2, 0);
             translateCenterWorld = ConvertUnits.ToWorld(translateCenterDisplay);
+            
+            float screenWidth = ConvertUnits.ToWorld(viewport.Width);
+            float screenHeight = ConvertUnits.ToWorld(viewport.Height);
+            WorldProjection = Matrix.CreateOrthographicOffCenter(0f, screenWidth, screenHeight, 0f, 0f, 1f);
 
-            float worldWidth = ConvertUnits.ToWorld(viewport.Width);
-            float worldHeight = ConvertUnits.ToWorld(viewport.Height);
-            WorldProjection = Matrix.CreateOrthographicOffCenter(0f, worldWidth, worldHeight, 0f, 0f, 1f); 
+            UpdateMatrices();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            UpdateMatrices();
         }
 
         public void Move(Vector2 amount)
@@ -64,17 +71,41 @@ namespace Foundation.Camera
             Position += amount;
         }
 
-        public override void Update(GameTime gameTime)
+        /// <summary>
+        /// Convert from screen position to physics world position
+        /// </summary>
+        /// <param name="screen">Position on screen</param>
+        /// <returns>Position on physics world</returns>
+        public Vector2 ScreenToWorld(Vector2 screen)
         {
-            Vector3 pos = new Vector3(Position, 0);
+            Vector3 pos = new Vector3(screen, 0);
+            pos = Game.GraphicsDevice.Viewport.Unproject(pos, WorldProjection, WorldView, Matrix.Identity);
+            return new Vector2(pos.X, pos.Y);
+        }
+
+        /// <summary>
+        /// Convert from physics world position to screen position
+        /// </summary>
+        /// <param name="screen">Position on screen</param>
+        /// <returns>Position on physics world</returns>
+        public Vector2 WorldToScreen(Vector2 world)
+        {
+            Vector3 pos = new Vector3(world, 0);
+            pos = Game.GraphicsDevice.Viewport.Unproject(pos, WorldProjection, DisplayView, Matrix.Identity);
+            return new Vector2(pos.X, pos.Y);
+        }
+
+        private void UpdateMatrices()
+        {
+            Vector3 pos = new Vector3(-Position, 0);
             Matrix zoomMat = Matrix.CreateScale(Scale);
 
             WorldView = Matrix.CreateTranslation(pos) *
-                        Matrix.CreateTranslation(translateCenterWorld) *
+                        Matrix.CreateTranslation(translateCenterWorld / Scale) *
                         zoomMat;
 
             DisplayView = Matrix.CreateTranslation(ConvertUnits.ToDisplay(pos)) *
-                          Matrix.CreateTranslation(translateCenterDisplay) *
+                          Matrix.CreateTranslation(translateCenterDisplay / Scale) *
                           zoomMat;
         }
     }
